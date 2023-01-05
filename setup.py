59e94f61b5840cdfce13e66728a7e504a3d61b74
kshitijrajsharma <skshitizraj@gmail.com>
2023-01-06 01:04:08 +0545

    added dataframe image

--- setup.py
+++ setup.py
@@ -19,5 +19,5 @@ setup(
     license="MIT",
     packages=["osmsg"],
     entry_points={"console_scripts": ["osmsg = osmsg.app:main"]},
-    install_requires=["osmium", "pandas", "requests"],
+    install_requires=["osmium", "pandas", "requests", "dataframe-image"],
 )
