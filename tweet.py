24d8354343237fa6ac45af431a1b57fc4e661c9d
kshitijrajsharma <skshitizraj@gmail.com>
2023-01-22 12:27:44 +0545

    test bold tweet

--- tweet.py
+++ tweet.py
@@ -36,7 +36,7 @@ def main():
         df = pd.read_csv(csv_file)
 
         # Get the attribute of first row
-        summary_text = f"{df.loc[0, 'name']} tops the table with {df.loc[0, 'map_changes']},followed by {df.loc[1, 'name']} with {df.loc[1, 'map_changes']} and {df.loc[2, 'name']} with {df.loc[2, 'map_changes']} map changes."
+        summary_text = f"*{df.loc[0, 'name']}* tops the table with {df.loc[0, 'map_changes']},followed by *{df.loc[1, 'name']}* with {df.loc[1, 'map_changes']} and *{df.loc[2, 'name']}* with *{df.loc[2, 'map_changes']}* map changes"
 
     filename = os.path.basename(first_file)
 
@@ -53,38 +53,38 @@ def main():
     if args.tweet_last_week:
         if args.tweet_global:
             api.update_status(
-                status=f"Global Contributors Last Week (UTC Timezone)\n(From {lstfile[1]} to {lstfile[2][:-4]})\n{summary_text}\nFull stats: https://github.com/kshitijrajsharma/OSMSG/tree/master/stats/Global/Weekly/weekly_global_stats.csv #weeklystats #OpenStreetMap #global",
+                status=f"*Global* Contributors Last Week (UTC)\n(From {lstfile[1]} to {lstfile[2][:-4]}\n{summary_text}\nFull stats: https://github.com/kshitijrajsharma/OSMSG/tree/master/stats/Global/Weekly/weekly_global_stats.csv #weeklystats #OpenStreetMap #global",
                 media_ids=[media.media_id],
             )
 
         else:
             api.update_status(
-                status=f"Nepal Contributors This week (UTC Timezone)\n(From {lstfile[1]} to {lstfile[2][:-4]})\n{summary_text}\nCheck full stats: https://github.com/kshitijrajsharma/OSMSG/tree/master/stats/Nepal/Weekly/weekly_nepal_stats.csv #weeklystats #OpenStreetMap #osmnepal",
+                status=f"*Nepal* Contributors This Week (UTC)\n(From {lstfile[1]} to {lstfile[2][:-4]}\n{summary_text}\nCheck full stats: https://github.com/kshitijrajsharma/OSMSG/tree/master/stats/Nepal/Weekly/weekly_nepal_stats.csv #weeklystats #OpenStreetMap #osmnepal",
                 media_ids=[media.media_id],
             )
         print("twitted")
     if args.tweet_last_month:
         if args.tweet_global:
             api.update_status(
-                status=f"Global Contributors Last Month (UTC Timezone)\n(From {lstfile[1]} to {lstfile[2][:-4]})\n{summary_text}\nFull stats: https://github.com/kshitijrajsharma/OSMSG/tree/master/stats/Global/Monthly/Monthly_global_stats.csv #monthlystats #OpenStreetMap #global",
+                status=f"*Global* Contributors Last Month (UTC)\n(From {lstfile[1]} to {lstfile[2][:-4]}\n{summary_text}\nFull stats: https://github.com/kshitijrajsharma/OSMSG/tree/master/stats/Global/Monthly/Monthly_global_stats.csv #monthlystats #OpenStreetMap #global",
                 media_ids=[media.media_id],
             )
 
         else:
             api.update_status(
-                status=f"Nepal Contributors This Month (UTC Timezone)\n(From {lstfile[1]} to {lstfile[2][:-4]})\n{summary_text}\nFull stats: https://github.com/kshitijrajsharma/OSMSG/tree/master/stats/Nepal/Monthly/Monthly_nepal_stats.csv #monthlyystats #OpenStreetMap #osmnepal",
+                status=f"*Nepal* Contributors This Month (UTC)\n(From {lstfile[1]} to {lstfile[2][:-4]}\n{summary_text}\nFull stats: https://github.com/kshitijrajsharma/OSMSG/tree/master/stats/Nepal/Monthly/Monthly_nepal_stats.csv #monthlyystats #OpenStreetMap #osmnepal",
                 media_ids=[media.media_id],
             )
         print("twitted")
     if args.tweet_last_day:
         if args.tweet_global:
             api.update_status(
-                status=f"Global Contributors Last Day (UTC Timezone)\n(From {lstfile[1]} to {lstfile[2][:-4]})\n{summary_text}\nFull stats: https://github.com/kshitijrajsharma/OSMSG/tree/master/stats/Global/Daily/daily_global_stats.csv #dailystats #OpenStreetMap #global",
+                status=f"*Global* Contributors Last Day (UTC)\n(From {lstfile[1]} to {lstfile[2][:-4]}\n{summary_text}\nFull stats: https://github.com/kshitijrajsharma/OSMSG/tree/master/stats/Global/Daily/daily_global_stats.csv #dailystats #OpenStreetMap #global",
                 media_ids=[media.media_id],
             )
         else:
             api.update_status(
-                status=f"Nepal Contributors Last Day (Nepal Tmz)\nFrom {lstfile[1]} - {lstfile[2][:-4]})\n{summary_text}\nFull Stats: https://github.com/kshitijrajsharma/OSMSG/tree/master/stats/Nepal/Daily/daily_nepal_stats.csv #dailystats #OpenStreetMap #osmnepal",
+                status=f"*Nepal* Contributors Last Day \nFrom {lstfile[1]} - {lstfile[2][:-4]}\n{summary_text}\nFull Stats: https://github.com/kshitijrajsharma/OSMSG/tree/master/stats/Nepal/Daily/daily_nepal_stats.csv #dailystats #OpenStreetMap #osmnepal",
                 media_ids=[media.media_id],
             )
             print("twitted")
