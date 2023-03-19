5d1a96a60998bccdc170d6d2a01111829efe9bce
kshitijrajsharma <skshitizraj@gmail.com>
2023-03-19 10:45:48 +0545

    0.1.26

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with io.open("README.md", encoding="utf-8") as f:
 
 setup(
     name="osmsg",
-    version="0.1.25",
+    version="0.1.26",
     description="OpenStreetMap Stats Generator : Commandline",
     long_description=readme,
     long_description_content_type="text/markdown",
