3c5c5fb1e034cdd6cb0277117e54adc4d570f49f
Mahesh-wor 'Invoron <72002075+mahesh-naxa@users.noreply.github.com>
2023-10-03 13:22:01 +0545

    Feat frontend deploy (#56)
    * feat: deployment test
    
    * feat: test
    
    * Update nextjs.yml
    
    * fix: lint
    
    * Update nextjs.yml
    
    * Update nextjs.yml
    
    * Update nextjs.yml
    
    * fix: remove package-lock for githubactions
    
    * fix: test deploy
    
    * fix: next js new yaml file
    
    * fix: test 2
    
    * fix: test fix
    
    * fix: test
    
    * fix : test 3
    
    * fix: test 11
    
    * test
    
    * test sds
    
    * fix: test3
    
    * Feat: added multiple chart from github
    
    * feat: added config for output:export
    
    * fix: config file
    
    * fix: test
    
    * Update nextjs.yml
    
    * updated nextjs github actions
    
    ---------
    
    Co-authored-by: Varun <deepak.pradhan048@gmail.com>
    Co-authored-by: Deepak Pradhan (Varun) <37866666+varun2948@users.noreply.github.com>

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,48 +3,89 @@
 
 import React, { useEffect, useState } from "react";
 import SimpleLineChart from "@/components/Charts/SimpleLineChart";
-import * as XLSX from 'xlsx';
+import * as XLSX from "xlsx";
 
 const Home = () => {
   const [data, setData] = useState<any | null>(undefined);
   const [csvData, setCsvData] = useState<any | null>(undefined);
+  const [csvDataNepal, setCsvDataNepal] = useState<any | null>(undefined);
   useEffect(() => {
-    const fetchFile = async () => {
+    const fetchFileForTurkey = async () => {
       try {
-        const response = await fetch('stats_summary.csv'); // Replace 'example.xls' with your file name
+        const response = await fetch(
+          "https://raw.githubusercontent.com/kshitijrajsharma/OSMSG/master/stats/turkeyeq/Daily/stats_summary.csv"
+        ); // Replace 'example.xls' with your file name
         const responseData = await response.arrayBuffer();
-        const workbook = XLSX.read(new Uint8Array(responseData), { type: 'array' });
+        const workbook = XLSX.read(new Uint8Array(responseData), {
+          type: "array",
+        });
         const sheetName = workbook.SheetNames[0];
         const worksheet = workbook.Sheets[sheetName];
         const jsonData = XLSX.utils.sheet_to_json(worksheet, {
           raw: false,
           // dateNF: "dd/mm/YYYY"
         });
-        setCsvData(jsonData);
+        const normalizedDatax = jsonData?.map((row: any) => {
+          if (row.editors === undefined) return {};
+          const parsedEditors = JSON?.parse(row.editors);
+          const keys = Object.keys(parsedEditors);
+          return {
+            name: row.timestamp,
+            josm: parsedEditors.josm,
+            rapid: parsedEditors["rapid "],
+            id: parsedEditors["id "],
+            users: +row["users"],
+          };
+        });
+        setCsvData(normalizedDatax);
       } catch (error) {
-        console.log(error, 'error');
-        console.error('Error fetching or reading the file:', error);
+        console.log(error, "error");
+        console.error("Error fetching or reading the file:", error);
+      }
+    };
+    const fetchFileForNepal = async () => {
+      try {
+        const response = await fetch(
+          "https://raw.githubusercontent.com/kshitijrajsharma/OSMSG/master/stats/Nepal/Weekly/stats_summary.csv"
+        ); // Replace 'example.xls' with your file name
+        const responseData = await response.arrayBuffer();
+        const workbook = XLSX.read(new Uint8Array(responseData), {
+          type: "array",
+        });
+        const sheetName = workbook.SheetNames[0];
+        const worksheet = workbook.Sheets[sheetName];
+        const jsonData = XLSX.utils.sheet_to_json(worksheet, {
+          raw: false,
+          // dateNF: "dd/mm/YYYY"
+        });
+        const normalizedDatax = jsonData?.map((row: any) => {
+          if (row.editors === undefined) return {};
+          const parsedEditors = JSON?.parse(row.editors);
+          const keys = Object.keys(parsedEditors);
+          return {
+            name: row.timestamp,
+            josm: parsedEditors.josm,
+            rapid: parsedEditors["rapid "],
+            id: parsedEditors["id "],
+            users: +row["users"],
+          };
+        });
+        setCsvDataNepal(normalizedDatax);
+      } catch (error) {
+        console.log(error, "error");
+        console.error("Error fetching or reading the file:", error);
       }
     };
 
-    fetchFile();
+    fetchFileForTurkey();
+    fetchFileForNepal();
   }, []);
-
-  useEffect(() => {
-    if (!csvData) return;
-    const normalizedDatax = csvData?.map((row: any) => {
-      if (row.editors === undefined) return {};
-      const parsedEditors = JSON?.parse(row.editors);
-      const keys = Object.keys(parsedEditors);
-      return { name: row.timestamp, josm: parsedEditors.josm, rapid: parsedEditors['rapid '], id: parsedEditors['id '], users: +row['users'] };
-    });
-    setData(normalizedDatax)
-
-  }, [csvData])
 
   return (
     <div>
-      <SimpleLineChart data={data} />;
+      <SimpleLineChart data={csvData} chartTitle="Daily Stats for Turkey" />;
+      <SimpleLineChart data={csvDataNepal} chartTitle="Daily Stats for Nepal" />
+      ;
     </div>
   );
 };
